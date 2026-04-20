1cb0cd7c9445fe1a11395e6696b105a30cf172ed
Eric Moore <moore.eric303@gmail.com>
2026-04-20 10:52:34 -0600

    update map

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -23,7 +23,7 @@ export default function ContactPage() {
       {/* Map */}
       <div className="w-full aspect-video overflow-hidden">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3031.774542446426!2d-112.00118562348236!3d40.54656994772587!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86117377f9920f7b%3A0xeddfba607b621928!2sPip!5e0!3m2!1sen!2sus!4v1758050203329!5m2!1sen!2sus"
+          src="https://maps.google.com/maps?ll=40.66,-111.93&z=10&t=m&output=embed"
           width="100%"
           height="100%"
           style={{ border: 0 }}
